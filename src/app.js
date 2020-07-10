5a5d21cccc602fa80b3a3b2ae6b05ef8461ab756
Vinicius Mazon <vnpmazon@gmail.com>
2020-07-10 20:54:45 -0300

    Add render previous messages

--- src/app.js
+++ src/app.js
@@ -20,8 +20,11 @@ const messages = [];
 io.on('connection', (socket) => {
   console.log(`socket conectado ${socket.id}`);
 
+  socket.emit('previousMessages', messages);
+
   socket.on('sendMessage', (data) => {
     messages.push(data);
+    socket.broadcast.emit('receivedMessage', data);
   });
 });
 
